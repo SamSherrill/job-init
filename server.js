68ab8cbd215f87df6be566bcf8ba103b49e39aec
SamSherrill <38333695+SamSherrill@users.noreply.github.com>
2020-05-07 14:06:11 -0400

    react app created in new client folder, server.js & package.json in main folder updated to mostly match Jonathan's boilerplate

--- server.js
+++ server.js
@@ -1,1 +1,31 @@
 console.log("Travis, please don't hate me");
+
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+    currentPort: PORT,
+  });
+});
+
+// app.use(express.static("client/build"));
+app.use(express.static("client/public"));
+
+app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "/client/build/index.html"));
+  res.sendFile(path.join(__dirname, "/client/public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express App is running on http://localhost:${PORT}`);
+});
