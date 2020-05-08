6b07c803f33cb922417efb9cc2157bbff3d26b2f
Stephanie smith <smithse4@gmail.com>
2020-05-08 11:29:15 -0400

    added Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
