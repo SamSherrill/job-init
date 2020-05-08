bce06f809df5a615f39683f3927e39488d9da079
Stephanie smith <smithse4@gmail.com>
2020-05-08 11:37:31 -0400

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home/Home"
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar/>
+      <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route component={NoMatch} />
+          </Switch>
       <Footer/>
-    </div>
+    </Router>
   );
 }
 
