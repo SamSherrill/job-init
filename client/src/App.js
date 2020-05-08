9003775f9519fc72fc548c85a14092e512ecd54a
Stephanie smith <smithse4@gmail.com>
2020-05-08 11:21:36 -0400

    added Bootstrap link and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Navbar from "./components/Navbar/Navbar"
 
 function App() {
   return (
     <div>
-      <h1>job init</h1>
+      <Navbar/>
     </div>
   );
 }
---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar navbar-dark bg-dark">
+      <span class="navbar-brand mb-0 h1" id="brand-format">
+        job <span id="brand-two">init</span>
+      </span>
+    </nav>
+  );
+};
+
+export default Navbar;
