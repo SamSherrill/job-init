da8698faaa60e2f11131c1cc6e23ad630b6715d9
Stephanie smith <smithse4@gmail.com>
2020-05-08 14:16:14 -0400

    updated home page to get routes working

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import './App.css';
-import Navbar from "./components/Navbar/Navbar"
-import Footer from "./components/Footer/Footer"
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home/Home"
+import Home from "./pages/Home/Home";
+import NoMatch from "./pages/NoMatch/NoMatch";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/home" component={Home} />
-            <Route component={NoMatch} />
-          </Switch>
-      <Footer/>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/home" component={Home} />
+          <Route component={NoMatch} />
+        </Switch>
+        <Footer />
+      </div>
     </Router>
   );
 }
