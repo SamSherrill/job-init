c6a693e1e2308440e2caed326934d63a11cedd0e
Stephanie smith <smithse4@gmail.com>
2020-05-11 09:24:30 -0400

    added buttons to home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import stock from "../../img/stock.jpeg";
+import "./Home.css"
 
 const Home = () => {
-    return (
-        <div>
-            <p>Home</p>
+  return (
+    <>
+      <div class="jumbotron jumbotron-fluid home-hero">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1 class="display-4" id="brand-format">
+                job <span id="brand-two">init</span>
+              </h1>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="container">
+        <div className="row">
+          <button className="btn home-button">Login</button>
+          <button className="btn home-button">Sign Up</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
