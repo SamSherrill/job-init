176df21686b976c4d11181dd1d70174e04e5e486
Stephanie smith <smithse4@gmail.com>
2020-05-11 10:26:02 -0400

    added temp hero image and temp text

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,23 +1,33 @@
 import React from "react";
 import stock from "../../img/stock.jpeg";
-import "./Home.css"
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <div class="jumbotron jumbotron-fluid home-hero">
+      <div class="home-hero">
         <div className="container">
           <div className="row">
             <div className="col">
-              <h1 class="display-4" id="brand-format">
+              {/* <h1 class="display-4" id="brand-format">
                 job <span id="brand-two">init</span>
-              </h1>
+              </h1> */}
             </div>
           </div>
         </div>
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row justify-content-center">
+          <h1 className="display-4" id="brand-format">
+            job <span id="brand-two">init</span>
+          </h1>
+        </div>
+        <div className="row justify-content-center">
+          <h6 className="lead">
+            Personalized job listings exclusively for junior developers
+          </h6>
+        </div>
+        <div className="row justify-content-center">
           <button className="btn home-button">Login</button>
           <button className="btn home-button">Sign Up</button>
         </div>
