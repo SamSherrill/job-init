d7efdef652b3d2e5b1527e28134e0947b97676dc
Jolie Guerra <Jolie_guerra@yahoo.com>
2020-05-11 18:11:22 -0400

    added login page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,10 +1,30 @@
 import React from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-const FormPage = () => {
+function Login() {
   return (
-      <div>Login</div>
-    
-  );
-};
+    <Form
+      className="container mt-5"
+      style={{ border: "2px solid gray", padding: "30px" }}
+    >
+      <Form.Group controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
 
-export default FormPage;
+      <Form.Group controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
+
+export default Login;
