7a06c444b6a1c3bc0bf914ef2602efa95ded4ab4
Stephanie smith <smithse4@gmail.com>
2020-05-12 11:12:37 -0400

    added video file

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,29 +1,39 @@
 import React from "react";
 import "./Home.css";
+import Hero from "../../img/hero-video.mov";
 
 const Home = () => {
+
+  // function hero() {
+  //   $("#video").click(function() {
+  //     var Video1 = $("#my_video_1").get(0);
+  //     Video1.play();
+  // });
+  // }
   return (
     <>
       <div class="home-hero">
+        {/* <div id="video">
+    <video id="my_video_1" class="video-js vjs-default-skin" 
+     preload="auto" width="501" height="282" poster={Hero} onclick="this.play();"controls
+     data-setup="{}">
+     <source src="video/final.webm" type='video/webm'/>
+     <source src="video/final.mp4" type='video/mp4'/>
+    </video>
+      </div> */}
         <div className="container">
           <div className="row">
-            <div className="col">
-              {/* <h1 class="display-4" id="brand-format">
-                job <span id="brand-two">init</span>
-              </h1> */}
-            </div>
+            <div className="col"></div>
           </div>
         </div>
       </div>
       <div className="container">
-          <br></br>
-          <br></br>
+        <br></br>
+        <br></br>
         <div className="row justify-content-center">
-            
           <h1 className="display-4" id="brand-format">
             job <span id="brand-two">init</span>
           </h1>
-          
         </div>
 
         <div className="row justify-content-center">
