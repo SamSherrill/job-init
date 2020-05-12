20b800b8aa2b09eb6b5c6225117e4ec385e6a43e
Stephanie smith <smithse4@gmail.com>
2020-05-12 13:28:56 -0400

    added dashboard cards with styling and dummy data

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,25 +1,44 @@
 import React, { Component } from "react";
+import results from "../../results.json";
 import "./Dashboard.css";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 class Dashboard extends Component {
+  state = {
+    results,
+  };
+
   render() {
     return (
       <>
-      <br></br>
-      <div className="container">
-        <div className="card">
-          <h5 className="card-header">Featured</h5>
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">
-              With supporting text below as a natural lead-in to additional
-              content. 
-            </p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
+        <br></br>
+        <div className="container">
+          <h4 className="dash-heading">Custom Job Listings</h4>
         </div>
+
+        <div className="container">
+          {this.state.results.map((result) => (
+            <Card className="result-card">
+              <Card.Header as="h5" className="feature-bar">
+                {result.title}
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>{result.company}</Card.Title>
+                <Card.Text>{result.location}</Card.Text>
+                <hr></hr>
+                <Card.Text>{result.description}</Card.Text>
+                <div className="justify-content-center">
+                  <a href={result.link}>
+                    <Button className="btn listing-button ">
+                      Full Job Listing
+                    </Button>
+                  </a>
+                  <Button className="btn save-button">Save Job</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
         </div>
       </>
     );
