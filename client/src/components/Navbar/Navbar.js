db552a87a543f762623f3ad1b833e3ff53afb1a0
Stephanie smith <smithse4@gmail.com>
2020-05-13 11:21:51 -0400

    updated all href paths to use Link

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav class="navbar job-nav">
-      <a href="/">
+      <Link to="/">
       <span class="navbar-brand mb-0 h1" id="brand-format">
         job <span id="brand-two">init</span>
       </span>
-      </a>
+      </Link>
     </nav>
   );
 };
