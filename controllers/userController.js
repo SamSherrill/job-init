e98b6b12c2a1b9a0229c4437199c4103b47c6bdc
JaysonD <jaduff20@gmail.com>
2020-05-15 14:13:51 -0400

    post and get currently working

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../models");
+const userData = require("../models/user.js");
 
 router.post("/", (req, res) => {
   console.log("Hit the controller post route for /api/users");
-  res.json(req.body);
+  console.log(req.body);
+  db.userData
+    .create(req.body)
+    .then((dbUsers) => {
+      res.json(dbUsers);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/", (req, res) => {
+  console.log(userData);
+  db.userData
+    .find({})
+    .then((dbUsers) => {
+      res.json(dbUsers);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
