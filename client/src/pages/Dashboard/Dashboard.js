cab3d77c952180b699c788b4862b4c84dfbb4d99
Stephanie smith <smithse4@gmail.com>
2020-05-15 11:37:20 -0400

    cleared out unnecessary code

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import results from "../../results.json";
 import "./Dashboard.css";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
@@ -8,28 +7,8 @@ import axios from "axios";
 
 class Dashboard extends Component {
   state = {
-    jobResults: []
+    jobResults: [],
   };
-
-  // getJobResult = () => {
-  //   axios
-  //     .get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=e61b06cb&app_key=0f4b8fc49b430d828c3c2a0b28246429&results_per_page=20&what=javascript%20developer&content-type=application/json`)
-  //     .then((response) => {
-  //       console.log(response);
-  //       if (response.data && response.data.success && response.data.data) {
-  //         this.setState({
-  //           jobResults: response.data.data,
-  //         });
-  //       } else {
-  //         this.setState({
-  //           error: response.data.message,
-  //         });
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
 
   componentDidMount() {
     this.getJobResult();
@@ -37,12 +16,14 @@ class Dashboard extends Component {
 
   getJobResult = () => {
     axios
-      .get(`https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=e61b06cb&app_key=0f4b8fc49b430d828c3c2a0b28246429&results_per_page=20&what=javascript%20developer&content-type=application/json`)
+      .get(
+        `https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=e61b06cb&app_key=0f4b8fc49b430d828c3c2a0b28246429&results_per_page=20&what=javascript%20developer&content-type=application/json`
+      )
       .then((response) => {
         console.log(response.data.results);
-          this.setState({
-            jobResults: response.data.results,
-          });
+        this.setState({
+          jobResults: response.data.results,
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -61,15 +42,17 @@ class Dashboard extends Component {
           {this.state.jobResults.map((result) => (
             <Card className="result-card" key={result.id}>
               <Card.Header as="h5" className="feature-bar">
-                <div dangerouslySetInnerHTML={{__html:result.title}}></div>
-                {/* {result.title.replace("<strong>", "").replace("</strong>", "").replace("<strong>", "").replace("</strong>", "")} */}
+                <div dangerouslySetInnerHTML={{ __html: result.title }}></div>
               </Card.Header>
               <Card.Body>
                 <Card.Title>{result.company.display_name}</Card.Title>
                 <Card.Text>{result.location.display_name}</Card.Text>
                 <hr></hr>
                 <Card.Text>
-                <span dangerouslySetInnerHTML={{__html:result.description}}></span></Card.Text>
+                  <span
+                    dangerouslySetInnerHTML={{ __html: result.description }}
+                  ></span>
+                </Card.Text>
                 <div className="text-center">
                   <Link to={result.redirect_url}>
                     <Button className="btn listing-button ">
